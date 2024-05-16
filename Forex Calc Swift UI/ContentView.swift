19143352e3be0c2fe7f9676c303fd0cf8ce63c98
victor <victorkibichy>
2024-05-16 12:16:43 +0300

    Initial Commit

---
+++ Forex Calc Swift UI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Forex Calc Swift UI
+//
+//  Created by  Bouncy Baby on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Forex Calc Swift UI/Forex_Calc_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forex_Calc_Swift_UIApp.swift
+//  Forex Calc Swift UI
+//
+//  Created by  Bouncy Baby on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Forex_Calc_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
